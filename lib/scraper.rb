e272ddf2525c8b00ac5f933eee7a4b79906f017c
Mac Dowell Prosper <macdowell@noukod.com>
2018-10-10 20:12:04 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,14 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
+
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    binding.pry
+  end
   
-    def print_courses
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
